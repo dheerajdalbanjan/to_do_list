0b2f31e68555fe3fdb9afe2cdee5320a61b2d6f3
unknown <dheerajdalbanjan0403@gmail.com>
2022-07-01 23:43:57 +0530

    first release

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyparser = require("body-parser");
+const app = express();
+app.set("view engine" , "ejs")
+app.use(express.static('public'));
+app.use(bodyparser.urlencoded({extended:true}));
+var items = [];
+app.get("/" , (req, res)=>{
+    var date = new Date();
+    let day = "";
+    var options = {
+        weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' 
+    }
+    day = date.toLocaleDateString("en-US" , options);
+    res.render("list" , {day :day , newitem:items})
+})
+
+
+app.listen(4000 , ()=>{
+    console.log("The server is running at port 4000");
+})
+app.post("/" , (req , res)=>{
+    var item = req.body.newitem ;
+    items.push(item);
+    res.redirect("/");
+})
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./lists/list.ejs" , "./public/{html , js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
